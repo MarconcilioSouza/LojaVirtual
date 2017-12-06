6099ed1b11a51b423a8ed328ba2ec9ab504944b3
Marconcilio <marconcilio@hotmail.com>
2017-12-06 10:05:35 -0200

    inicio usando o core 2.0

---
+++ LojaVirtual.Core.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LojaVirtual.Core.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
